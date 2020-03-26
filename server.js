e3f9b7063ff215ddba060ce0d67c1750a5bc3dcc
Justin <jlkellogg18@outlook.com>
2020-03-26 09:51:45 -0700

    added exercise path

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
