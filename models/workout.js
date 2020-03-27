db4e086002a39baa0ac58b68e4438489401f62bc
justinkellogg89 <jlkellogg18@outlook.com>
2020-03-27 12:12:08 -0700

    fixed workout model

--- models/workout.js
+++ models/workout.js
@@ -1,22 +1,21 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
 const WorkoutSchema = new Schema({
   day: {
     type: Date
   },
-
-  exercises: [{
-    type: String,
-    name: String,
-    weight: Number,
-    sets: Number,
-    reps: Number,
-    duration: Number
-  }]
-})
-
+  exercises: [
+    {
+      type: {
+        type: String
+      },
+      name: String,
+      weight: Number,
+      sets: Number,
+      reps: Number,
+      duration: Number
+    }
+  ]
+});
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
 module.exports = Workout;
